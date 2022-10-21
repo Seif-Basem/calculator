dce9dc6d3e08d84ce19a5369a02be7b2530ed095
Seif Basem <103323488+Seif-Basem@users.noreply.github.com>
2022-10-21 08:31:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let buttons = document.querySelector(".buttons");
+let btn = document.querySelectorAll("span");
+let value = document.getElementById("value");
+let toggleBtn = document.querySelector(".toggleBtn"); //dark mode
+let body = document.querySelector("body"); //dark mode
+
+
+for(let i = 0; i < btn.length; i++){
+    btn[i].addEventListener("click", function() {
+        if(this.innerHTML == "=") {
+            value.innerHTML = eval(value.innerHTML);
+        }else {
+            if(this.innerHTML == "Clear") {
+                value.innerHTML = "";
+            }
+            else {
+                value.innerHTML += this.innerHTML;
+            }
+        }
+    })
+}
+
+//dark mode
+toggleBtn.onclick = function () {
+    body.classList.toggle("dark");
+}
